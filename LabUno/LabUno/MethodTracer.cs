ddf35420f3593dc677a40c985e669e61e5c78043
Azam Alamov <alamovazamjon@gmail.com>
2024-02-27 15:02:19 -0800

    initial commit

---
+++ LabUno/LabUno/MethodTracer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+public interface ITracer
+{
+    void StartTrace();
+    void StopTrace();
+}
+
+public class MethodTracer : ITracer
+{
+    private Stopwatch stopwatch;
+    private TraceResult traceResult;
+
+    public void StartTrace()
+    {
+        stopwatch = new Stopwatch();
+        traceResult = new TraceResult();
+        stopwatch.Start();
+    }
+
+    public void StopTrace()
+    {
+        stopwatch.Stop();
+        TimeSpan elapsed = stopwatch.Elapsed;
+        traceResult.ElapsedMilliseconds = elapsed.TotalMilliseconds;
+
+        StackTrace stackTrace = new StackTrace();
+        MethodBase methodBase = stackTrace.GetFrame(1).GetMethod();
+        traceResult.MethodName = methodBase.Name;
+        traceResult.ClassName = methodBase.DeclaringType.Name;
+
+        PrintTraceResult();
+    }
+
+    private void PrintTraceResult()
+    {
+        Console.WriteLine($"Method: {traceResult.MethodName}");
+        Console.WriteLine($"Class: {traceResult.ClassName}");
+        Console.WriteLine($"Execution time: {traceResult.ElapsedMilliseconds} ms");
+    }
+}
+
+public class TraceResult
+{
+    public string MethodName { get; set; }
+    public string ClassName { get; set; }
+    public double ElapsedMilliseconds { get; set; }
+}
+
---
+++ LabUno/LabUno/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        ITracer tracer = new MethodTracer();
+
+        tracer.StartTrace();
+        ExampleMethod();
+        tracer.StopTrace();
+    }
+
+    static void ExampleMethod()
+    {
+        for (int i = 0; i < 100000000; i++)
+        {
+            // Do some computation
+        }
+    }
+}
